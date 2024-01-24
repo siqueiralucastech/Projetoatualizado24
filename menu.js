95e2e830295bbdc6223bcde5185d681481577376
siqueiralucastech <153107393+siqueiralucastech@users.noreply.github.com>
2024-01-24 16:51:36 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,19 @@
+/* Desenvolvido por Lucas Pinheiro De Carvalho Siqueira */
+
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu-mobile");
+let overlay = document.getElementById("overlay-menu");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+/* Desenvolvido por Lucas Pinheiro De Carvalho Siqueira */
